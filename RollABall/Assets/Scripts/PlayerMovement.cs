16c97b6fa3f53c274220c8dfcc1ee99406985933
Greenflex <jsos30@icloud.com>
2022-02-03 17:41:01 -0800

    RollABall1.1
    Added movability, point-pickups, and a ramp.
    Currently, the trigger for removing the pickups do not work, and I have yet to add a score counter and score function.

---
+++ RollABall/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Vector3 moveInput;
+    private Rigidbody rb;
+    private PlayerCamera playerCam;
+    [SerializeField] private float speed;
+    [SerializeField] private float jumpForce;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        playerCam = FindObjectOfType<PlayerCamera>();
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    private void Update()
+    {
+        moveInput = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = new Vector3(rb.velocity.x, 0, rb.velocity.z);
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+
+    private void MovePlayer()
+    {
+        transform.forward = playerCam.transform.forward;
+        Vector3 directionX = transform.right.normalized * moveInput.x;
+        Vector3 directionZ = transform.forward.normalized * moveInput.z;
+
+        rb.velocity = new Vector3(0, rb.velocity.y,0) + (directionX + directionZ);
+    }
+}
+
