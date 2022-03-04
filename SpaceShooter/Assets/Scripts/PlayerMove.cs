f604b82decf205a8720400da8025e3a82b247806
Greenflex <jsos30@icloud.com>
2022-03-03 19:07:37 -0800

    Initiated the space shooter project
    This time, the project is separated from the RollABall directory, since this project will be using separate scenes.

---
+++ SpaceShooter/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    // velocity and key input
+    private Vector2 moveInput;
+    private Rigidbody rb;
+    
+    // the default speed of player
+    [SerializeField] private float speed = 5f;
+
+    private void Awake()
+    {
+        // Getting Rigidbody from Inspector
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        // class is called for constant movement
+        MovePlayer();
+    }
+
+    private void Update()
+    {
+        // key input is received here
+        moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+    }
+
+    private void MovePlayer()
+    {
+        // ensures movement for x and y directions
+        Vector2 directionX = transform.right.normalized * moveInput.x;
+        Vector2 directionY = transform.forward.normalized * moveInput.y;
+
+        rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y) + (directionX + directionY);
+    }
+}
